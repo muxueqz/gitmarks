25257768618b27d0c1e5cdc5c9a939659f215df6
Alex Petrosh <petrosh@users.noreply.github.com>
2016-06-10 22:11:17 +0200

    Corrections

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var hostArray = window.location.host.split('.'),
     fullName = username + '/' + repository,
     issuesHref = 'https://github.com/' + fullname,
     issuesElement = document.getElementById('issues'),
-    bookmarkElement = document.getElementById('bookmark'),
-    issuesElement.href = issuesHref,
-    bookmarkElement.href='javascript:(function(s){try{s=document.selection.createRange().text}catch(_){s=document.getSelection()}location.href="https://github.com/'+fullname+'/issues/new?title="+document.title+"&body="+location+String.fromCharCode(37)+"0A---"+String.fromCharCode(37)+"0A"+encodeURIComponent(s)})()';
+    bookmarkElement = document.getElementById('bookmark');
+
+    issuesElement.href = issuesHref;
+    bookmarkElement.href='javascript:(function(s){try{s=document.selection.createRange().text}catch(_){s=document.getSelection()}location.href="https://github.com/'+fullName+'/issues/new?title="+document.title+"&body="+location+String.fromCharCode(37)+"0A---"+String.fromCharCode(37)+"0A"+encodeURIComponent(s)})()';
